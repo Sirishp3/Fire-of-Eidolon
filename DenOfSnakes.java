550619f87dc5dff6281cbabedd910fa97cd5f0da
Sirish Patibanda <63757721+Sirishp3@users.noreply.github.com>
2022-10-28 13:44:57 -0500

    FIle changes

---
+++ DenOfSnakes.java
@@ -0,0 +1,26 @@
+package sample;
+
+public class DenOfSnakes implements ChamberTile{
+    int row = -1;
+    int column = -1;
+
+    @Override
+    public int getRow() {
+        return row;
+    }
+
+    @Override
+    public int getColumn() {
+        return column;
+    }
+
+    @Override
+    public void setRow(int r) {
+        row = r;
+    }
+
+    @Override
+    public void setColumn(int c) {
+        column = c;
+    }
+}
---
+++ Vestibule.java
@@ -0,0 +1,17 @@
+package sample;
+
+
+public class Vestibule  {
+    int row = 4;
+    int column = 4;
+
+
+    public int getRow() {
+        return row;
+    }
+
+
+    public int getColumn() {
+        return column;
+    }
+}
